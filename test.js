76dab08cb4091aa331cd4c3e37bafa2bf4479bcb
Andrew Kelley <superjoe30@gmail.com>
2013-11-29 23:57:08 -0500

    more test

--- test.js
+++ test.js
@@ -16,11 +16,39 @@ var c = {
   "goats": [1, 2, 1],
 };
 
+var d = {
+  "layer1": {
+    "layer2": {
+      "layer3": {
+        "layer4": {
+          "layer5": "hi"
+        },
+      },
+    },
+  },
+};
+
+var e = {
+  "layer1": {
+    "layer2": {
+      "layer3": {
+        "layer5": {
+          "layer6": "hi"
+        },
+      },
+    },
+  },
+};
+
 assertDeepEqual(a, b, "foobar");
 
 assert.throws(function() {
   assertDeepEqual(a, c, "foobar");
 }, /foobar: Property,goats,ArrayIndex,2,Primitive,3,1/);
+
+assert.throws(function() {
+  assertDeepEqual(d, e);
+}, /Property,layer1,Property,layer2,Property,layer3,Property,layer4,Type,object,undefined/);
 
 assert.throws(function() {
   assertDeepEqual(new Date(), 1);
